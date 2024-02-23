c039eccec4c4fd99d1126499b53bb0b5c29af0f5
Jonathan Hallstrom <lmj.hallstrom@gmail.com>
2024-02-23 23:06:13 +0100

    make basic version using std.pdq

--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,66 @@
 const std = @import("std");
 
-pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+const T = i32;
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+const MAXN = 500_000;
+var buf: [MAXN * 16]u8 align(4) = undefined;
+var ints: [MAXN]i32 = undefined;
+var n: usize = 0;
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-    try bw.flush(); // don't forget to flush!
+fn less(_: @TypeOf(.{}), a: i32, b: i32) bool {
+    return a < b;
 }
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+pub fn main() !void {
+    _ = try std.io.getStdIn().reader().readAll(&buf);
+    var slice: []u8 = &buf;
+
+    while ('0' <= slice[0] and slice[0] <= '9') : (slice = slice[1..]) {
+        n = n * 10 + slice[0] - '0';
+    }
+
+    for (0..n) |i| {
+        var parsed: i32 = 0;
+        var sign: i32 = 1;
+        slice = slice[1..];
+        if (slice[0] == '-') {
+            slice = slice[1..];
+            sign = -1;
+        }
+        while ('0' <= slice[0] and slice[0] <= '9') : (slice = slice[1..]) {
+            parsed = parsed * 10 + slice[0] - '0';
+        }
+
+        ints[i] = parsed * sign;
+    }
+
+    std.sort.pdq(i32, ints[0..n], .{}, less);
+
+    slice = &buf;
+    for (ints[0..n]) |i| {
+        var x: i64 = i;
+        if (x < 0) {
+            x = -x;
+            slice[0] = '-';
+            slice = slice[1..];
+        }
+        var non_neg: u64 = @intCast(x);
+
+        var number_slice: []u8 = slice[0..20];
+        var digits_used: usize = 1;
+        number_slice[0] = @intCast(non_neg % 10 + '0');
+        non_neg /= 10;
+
+        while (non_neg > 0) {
+            number_slice[digits_used] = @intCast(non_neg % 10 + '0');
+            digits_used += 1;
+            non_neg /= 10;
+        }
+        std.mem.reverse(u8, number_slice[0..digits_used]);
+        slice = slice[digits_used..];
+
+        slice[0] = ' ';
+        slice = slice[1..];
+    }
+    try std.io.getStdOut().writer().writeAll(buf[0..buf.len - slice.len]);
 }
