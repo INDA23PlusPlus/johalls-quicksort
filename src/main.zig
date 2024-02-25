12b1f853a10beb6120d5202861311103a4ae7482
Jonathan Hallstrom <lmj.hallstrom@gmail.com>
2024-02-25 14:45:20 +0100

    implement basic lsb radix sort

--- src/main.zig
+++ src/main.zig
@@ -2,9 +2,9 @@ const std = @import("std");
 const builtin = @import("builtin");
 
 const MAXN = 500_000;
-var global_buf: [MAXN * 16]u8 align(4) = undefined;
+var global_buf: [MAXN * 16]u8 align(64) = undefined;
 var ints: [MAXN]i32 = undefined;
-var n: usize = 0;
+var glob_n: usize = 0;
 
 // x has to be a pointer
 fn as_bytes(x: anytype) []u8 {
@@ -94,18 +94,15 @@ test "write_1e4" {
 }
 
 fn write_1e4_full(buf: []u8, val: u32) void {
-    var t1: u32 = 0;
-    t1 += (val / 100) % 10 << 8;
-    t1 += (val / 100) / 10;
-    var t2: u32 = 0;
-    t2 += val % 10 << 8;
-    t2 += val / 10 % 10;
+    const t1: u32 = map_two(@intCast(val / 100));
+    const t2: u32 = map_two(@intCast(val % 100));
+
     var t: u32 = t1 + (t2 << 16);
 
     inline for (0..4) |e| {
         t += '0' << 8 * e;
     }
-    
+
     // call to memcpy wasnt being inlined
     // should compile to a single mov
     for (as_bytes(&t), 0..4) |b, i| {
@@ -131,7 +128,10 @@ test "write_1e4_full" {
     }
 }
 
-fn write_1e8(buf: []u8, val: u32) []u8 {
+inline fn write_1e8(buf: []u8, val: u32) []u8 {
+    if (val < 1e4) {
+        return write_1e4(buf, val);
+    }
     var ret = write_1e4(buf, val / 10_000);
     write_1e4_full(ret, val % 10_000);
     return ret[4..];
@@ -146,10 +146,10 @@ fn write_1e8_full(buf: []u8, val: u32) void {
 
 // note: may write up to three garbage values into buffer after the end
 fn write_fast(buf: []u8, val: u32) []u8 {
-    switch (builtin.target.cpu.arch.endian()) {
-        .big => return write_slow(buf, val),
-        else => {},
-    }
+    // switch (builtin.target.cpu.arch.endian()) {
+    //     .big => return write_slow(buf, val),
+    //     else => {},
+    // }
 
     if (val < 100) {
         return write_1e2(buf, val);
@@ -186,16 +186,199 @@ fn less(_: @TypeOf(.{}), a: i32, b: i32) bool {
     return a < b;
 }
 
+fn test_sort(sorting_fn: anytype, comptime T: type, comptime max_len: comptime_int) !void {
+    var nums: [max_len]T = undefined;
+    var check: [max_len]T = undefined;
+    var rand: T = 0;
+    rand +%= 637991;
+    const utils = struct {
+        fn less(_: @TypeOf(.{}), a: T, b: T) bool {
+            return a < b;
+        }
+    };
+    for (1..max_len + 1) |len| {
+        for (0..len) |i| {
+            rand *%= 13789;
+            rand +%= 19237;
+            nums[i] = rand;
+            check[i] = rand;
+        }
+
+        sorting_fn(nums[0..len]);
+        std.sort.pdq(T, check[0..len], .{}, utils.less);
+
+        for (0..len) |i| {
+            try std.testing.expectEqual(check[i], nums[i]);
+        }
+    }
+}
+
+fn test_sortf(sorting_fn: anytype, comptime T: type, comptime max_len: comptime_int) !void {
+    var nums: [max_len]T = undefined;
+    var check: [max_len]T = undefined;
+    var rand: usize = 0;
+    rand +%= 63799;
+    const utils = struct {
+        fn less(_: @TypeOf(.{}), a: T, b: T) bool {
+            return a < b;
+        }
+    };
+    for (1..max_len + 1) |len| {
+        for (0..len) |i| {
+            rand *%= 13789;
+            rand +%= 19237;
+            const t1: T = @floatFromInt(rand);
+
+            rand *%= 13789;
+            rand +%= 19237;
+            const t2: T = @floatFromInt(rand);
+
+            nums[i] = t1 + t2 / 1000.0;
+            check[i] = t1 + t2 / 1000.0;
+        }
+
+        sorting_fn(nums[0..len]);
+        std.sort.pdq(T, check[0..len], .{}, utils.less);
+
+        for (0..len) |i| {
+            try std.testing.expectEqual(check[i], nums[i]);
+        }
+    }
+}
+
+fn insertion_sort(arr: anytype) void {
+    for (1..arr.len) |i| {
+        const to_be_inserted = arr[i];
+        var j = i;
+        while (j > 0 and arr[j - 1] > to_be_inserted) : (j -= 1) {
+            arr[j] = arr[j - 1];
+        }
+        arr[j] = to_be_inserted;
+    }
+}
+
+test "insertion_sort" {
+    try test_sort(insertion_sort, u32, 1000);
+    try test_sort(insertion_sort, i32, 1000);
+    try test_sortf(insertion_sort, f32, 1000);
+    try test_sortf(insertion_sort, f64, 1000);
+}
+
+fn basic_radix_sort_unsigned_impl(comptime Log2NumBuckets: comptime_int, arr: anytype, out: anytype) void {
+    const Bits = @bitSizeOf(@TypeOf(arr[0]));
+    const NumBuckets = 1 << Log2NumBuckets;
+    const uvec = @Vector(NumBuckets, usize);
+    var counts: uvec = undefined;
+    const n = arr.len;
+
+    const iters = (Bits + Log2NumBuckets - 1) / Log2NumBuckets;
+    var a = arr;
+    var b = out;
+    for (0..iters) |offset| {
+        const shift = Log2NumBuckets * offset;
+        const mask = NumBuckets - 1;
+
+        counts = @splat(0);
+        for (a) |e| {
+            counts[(e >> @intCast(shift)) & mask] += 1;
+        }
+
+        for (1..NumBuckets) |i| {
+            counts[i] += counts[i - 1];
+        }
+
+        for (0..n) |r| {
+            const i = n - r - 1;
+            const c = &counts[(a[i] >> @intCast(shift)) & mask];
+
+            c.* -= 1;
+            b[c.*] = a[i];
+        }
+        const c = a;
+        a = b;
+        b = c;
+    }
+
+    if (iters % 2 == 1) {
+        @memcpy(arr, a);
+    }
+}
+
+fn SliceCast(comptime T: type, ptr: anytype, len: usize) []T {
+    const p: [*]T = @ptrCast(@alignCast(ptr));
+    return p[0..len];
+}
+
+fn radix_sort(arr: anytype) void {
+    const Bits = @bitSizeOf(@TypeOf(arr[0]));
+    const SortType = std.meta.Int(.unsigned, Bits);
+    const to_sort = SliceCast(SortType, arr.ptr, arr.len);
+
+    const FlipHighest = switch (@typeInfo(@TypeOf(arr[0]))) {
+        .Int => |info| switch (info.signedness) {
+            .signed => true,
+            .unsigned => false,
+        },
+        else => false,
+    };
+
+    const utils = struct {
+        fn flip(a: []SortType) void {
+            const VecSize = 16;
+            const Vec = @Vector(VecSize, SortType);
+            var s = a;
+
+            const mask = 1 << (Bits - 1);
+
+            while (s.len >= VecSize) : (s = s[VecSize..]) {
+                var tmp: Vec = s[0..VecSize].*;
+                tmp ^= @splat(mask);
+
+                for (0..VecSize) |i| {
+                    s[i] = tmp[i];
+                }
+            }
+
+            for (s) |*e| {
+                e.* ^= mask;
+            }
+        }
+    };
+
+    if (FlipHighest) {
+        // std.testing.expect(false) catch @panic("sad");
+        utils.flip(to_sort);
+    }
+
+    // we already have a global buffer thats big enough and aligned properly so why not use it
+    const out = SliceCast(SortType, &global_buf[0], arr.len);
+    basic_radix_sort_unsigned_impl(8, to_sort, out);
+
+    @memcpy(out, to_sort);
+
+    if (FlipHighest) {
+        utils.flip(to_sort);
+    }
+}
+
+test "radix_sort" {
+    try test_sort(radix_sort, u32, 10000);
+    try test_sort(radix_sort, i32, 10000);
+    try test_sortf(radix_sort, f32, 10000);
+    try test_sortf(radix_sort, f64, 10000);
+}
+
 pub fn main() !void {
     _ = try std.io.getStdIn().reader().readAll(&global_buf);
     var slice: []u8 = &global_buf;
-    // var timer = try std.time.Timer.start();
+    const print = false;
+    var timer = try std.time.Timer.start();
 
     while ('0' <= slice[0] and slice[0] <= '9') : (slice = slice[1..]) {
-        n = n * 10 + slice[0] - '0';
+        glob_n = glob_n * 10 + slice[0] - '0';
     }
 
-    for (0..n) |i| {
+    for (0..glob_n) |i| {
         var parsed: i32 = 0;
         var sign: i32 = 1;
         slice = slice[1..];
@@ -210,15 +393,21 @@ pub fn main() !void {
         ints[i] = parsed * sign;
     }
 
-    // std.debug.print("parsing input {}us\n", .{timer.read() / 1000});
-    // timer.reset();
+    if (print) {
+        std.debug.print("parsing input {}us\n", .{timer.read() / 1000});
+        timer.reset();
+    }
 
-    std.sort.pdq(i32, ints[0..n], .{}, less);
-    // std.debug.print("sorting {}us\n", .{timer.read() / 1000});
-    // timer.reset();
+    // std.sort.pdq(i32, ints[0..glob_n], .{}, less);
+    radix_sort(ints[0..glob_n]);
+
+    if (print) {
+        std.debug.print("sorting {}us\n", .{timer.read() / 1000});
+        timer.reset();
+    }
 
     slice = &global_buf;
-    for (ints[0..n]) |i| {
+    for (ints[0..glob_n]) |i| {
         var x: i64 = i;
         if (x < 0) {
             x = -x;
@@ -232,6 +421,9 @@ pub fn main() !void {
         slice = slice[1..];
     }
     try std.io.getStdOut().writer().writeAll(global_buf[0 .. global_buf.len - slice.len]);
-    // std.debug.print("parsing output {}us\n", .{timer.read() / 1000});
-    // timer.reset();
+
+    if (print) {
+        std.debug.print("printing output {}us\n", .{timer.read() / 1000});
+        timer.reset();
+    }
 }
